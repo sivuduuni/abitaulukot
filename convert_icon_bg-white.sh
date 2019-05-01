bbd9549f7f6793bb9a4355abf4b1bebf96cd6755
Sivuduuni Global Office <office@sivuduuni.biz>
2019-05-01 08:22:04 +0300

    Add some margin to iOS icons

--- convert_icon_bg-white.sh
+++ convert_icon_bg-white.sh
@@ -6,10 +6,23 @@ if [ -f $2 ]; then
   rm $2
 fi
 
-#convert $1 -density 300 -background none -resize $3 -gravity center -extent $3 $2
-inkscape --export-png=$2 \
+TEMPICON=`tempfile --suffix=.png`
+TEMPBG=`tempfile --suffix=.png`
+SCALEDPX=`echo "define trunc(x) { auto s; s=scale; scale=0; x=x/1; scale=s; return x } trunc($3 * 0.7)" | bc -l`
+
+# Resize icon to 90% of the desired size
+inkscape --export-png=$TEMPICON \
 	--export-dpi=200 \
 	--export-background="#ffffff" \
 	--export-background-opacity=255 \
-	--export-width=$3 --export-height=$3 \
+	--export-width=$SCALEDPX --export-height=$SCALEDPX \
 	--without-gui $1
+
+# Scale white background to the desired size
+convert -resize $3x$3 icon/white_100x100.png $TEMPBG
+
+# Merge background and icon
+convert $TEMPBG $TEMPICON -gravity center -composite -format png -quality 100 $2
+
+rm $TEMPICON
+rm $TEMPBG
